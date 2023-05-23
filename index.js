477058123ac814be7391a362d579adf86cc081ad
Deetalisman <ishmaeldotun@gmail.com>
2023-05-23 12:53:04 -0700

    added cart counter

--- index.js
+++ index.js
@@ -14,4 +14,27 @@ const cart = document.querySelector(".cart");
 const add = document.querySelector(".feet-right");
 add.addEventListener('click', function(e){
     cart.classList.toggle('hidden');
-})
+});
+
+//----------------increase counter--------------//
+const plus = document.querySelector("#add");
+const remove = document.querySelector("#remove");
+const outPut = document.querySelector(".out");
+let count = 0;
+
+function increase() {
+    count += 1;
+    outPut.textContent = count;
+};
+
+function decrease() {
+    if(count >= 1){
+        count -= 1;
+        outPut.textContent = count;
+    } else{
+        outPut.textContent = 0;
+
+    }
+   
+}
+
